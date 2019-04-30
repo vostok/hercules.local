12f1dcacf5c53a5ebdd45b1d1691ebb1273cb410
alexz <alexz@skbkontur.ru>
2019-04-30 16:33:44 +0500

    Added ConfigureAwait(false).

--- Vostok.Hercules.Local/HerculesDownloader.cs
+++ Vostok.Hercules.Local/HerculesDownloader.cs
@@ -44,7 +44,7 @@ namespace Vostok.Hercules.Local
                     {
                         log.Info($"Begin downloading {asset.Name}..");
 
-                        var response = await SendRequestAsync(Request.Get(asset.BrowserDownloadUrl), 2.Minutes());
+                        var response = await SendRequestAsync(Request.Get(asset.BrowserDownloadUrl), 2.Minutes()).ConfigureAwait(false);
                         if (response.Code != ResponseCode.Ok)
                             throw new Exception($"Request to {asset.BrowserDownloadUrl} failed with code = {response.Code}.");
 
